3f77a155e8117ea9517bb4ddea64ef87bbf39533
Subham Mondal <subhamoffi@gmail.com>
2026-01-18 19:51:22 +0530

    Taking user input

--- src/main.cpp
+++ src/main.cpp
@@ -9,4 +9,9 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   cout << "$ ";
+  std::string input;
+  std::getline(std::cin, input);
+  std::cout << input << ": command not found" << std::endl;
+
+  return 0;
 }
